7b742666b0aa831961bcd780b7954bccfedbef78
Dawulo <lajonowsky@gmail.com>
2023-09-27 18:13:25 +0200

    Retrieving search results and adding assertions

--- src/test/java/SearchHotel.java
+++ src/test/java/SearchHotel.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchHotel {
  @Test
@@ -19,17 +23,25 @@ public class SearchHotel {
      driver.findElement(By.xpath("//input[@id='input-button__locations-or-properties']")).sendKeys("Warszawa");
      driver.findElement(By.xpath("//div[2]//div[1]/hp-room-search-location-item[1]/div[1]")).click();
      driver.findElements(By.xpath("//div[@class='input-button__input ng-star-inserted']"))
-             .stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
-     driver.findElement(By.xpath("(//div[@class='calendar-body__cell' and @data-id='2023-10-23'])[2]")).click();
+             .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+     driver.findElements(By.xpath("//div[@class='calendar-body__cell' and @data-id='2023-10-23']"))
+             .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
      driver.findElements(By.xpath("(//div[@class='input-button__input ng-star-inserted'])[2]"))
-             .stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
+             .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
      driver.findElement(By.xpath("(//div[@class='calendar-body__cell' and @data-id='2023-10-27'])[2]")).click();
      driver.findElement(By.xpath("//div[@class='input-button__input input-button__display-value--truncate-text ng-star-inserted']")).click();
      driver.findElement(By.xpath("(//div[@class='counter__button-wrapper--enabled'])[3]")).click();
      driver.findElement(By.xpath("(//div[@class='counter__button-wrapper--enabled'])[6]")).click();
-     driver.findElement(By.xpath("(//select[@class='room-picker__select b2' and @name='childrenselect'])[2]")).sendKeys("4");
-     driver.findElement(By.xpath("(//button[@class='rooms_done-button ry-button--anchor-blue ry-button--anchor'])[2]")).click();
+     driver.findElements(By.xpath("//select[@class='room-picker__select b2' and @name='childrenselect']"))
+             .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(el -> el.sendKeys("4"));
+     driver.findElements(By.xpath("//button[@class='rooms_done-button ry-button--anchor-blue ry-button--anchor']"))
+             .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
      driver.findElement(By.xpath("//button[@class='rooms-search-widget__start-search ry-button--gradient-yellow']")).click();
-     driver.close();
+
+     List<String> hotels = driver.findElements(By.xpath("//h4[@class='accommodation-card-body__property-name c-very-dark-grey']"))
+             .stream().map(el -> el.getAttribute("textContent")).collect(Collectors.toList());
+
+  Assert.assertEquals(" EXCLUSIVE Aparthotel Warszawa ", hotels.get(0));
+  Assert.assertEquals(" Hostel Warszawa Powstaniec ", hotels.get(2));
  }
 }
