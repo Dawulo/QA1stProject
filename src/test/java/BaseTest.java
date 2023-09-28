38ba54d05db300c1a7f1924b12392f1c4b844c94
Dawulo <lajonowsky@gmail.com>
2023-09-28 15:51:31 +0200

    Adding additional tests and cleaning up the code

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.ryanair.com/pl/pl");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/SearchHotelTest.java
+++ src/test/java/SearchHotelTest.java
@@ -1,23 +1,13 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SearchHotelTest {
+public class SearchHotelTest extends BaseTest{
  @Test
-    public void getInfo() {
-     WebDriverManager.chromedriver().setup();
-     WebDriver driver = new ChromeDriver();
-     driver.manage().window().maximize();
-     driver.get("https://www.ryanair.com/pl/pl");
-     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
+    public void getInfoTest() {
      driver.findElement(By.xpath("//button[text()='Tak, zgadzam się']")).click();
      driver.findElement(By.xpath("//div[text()='hotele']")).click();
      driver.findElement(By.xpath("//input[@id='input-button__locations-or-properties']")).sendKeys("Warszawa");
@@ -43,5 +33,28 @@ public class SearchHotelTest {
 
   Assert.assertEquals(" EXCLUSIVE Aparthotel Warszawa ", hotels.get(0));
   Assert.assertEquals(" Hostel Warszawa Powstaniec ", hotels.get(2));
+ }
+
+ @Test
+ public void getInfoWithoutCityTest() {
+  driver.findElement(By.xpath("//button[text()='Tak, zgadzam się']")).click();
+  driver.findElement(By.xpath("//div[text()='hotele']")).click();
+  driver.findElements(By.xpath("//div[@class='input-button__input ng-star-inserted']"))
+          .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+  driver.findElements(By.xpath("//div[@class='calendar-body__cell' and @data-id='2023-10-23']"))
+          .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+  driver.findElements(By.xpath("(//div[@class='input-button__input ng-star-inserted'])[2]"))
+          .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+  driver.findElement(By.xpath("(//div[@class='calendar-body__cell' and @data-id='2023-10-27'])[2]")).click();
+  driver.findElement(By.xpath("//div[@class='input-button__input input-button__display-value--truncate-text ng-star-inserted']")).click();
+  driver.findElement(By.xpath("(//div[@class='counter__button-wrapper--enabled'])[3]")).click();
+  driver.findElement(By.xpath("(//div[@class='counter__button-wrapper--enabled'])[6]")).click();
+  driver.findElements(By.xpath("//select[@class='room-picker__select b2' and @name='childrenselect']"))
+          .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(el -> el.sendKeys("4"));
+  driver.findElements(By.xpath("//button[@class='rooms_done-button ry-button--anchor-blue ry-button--anchor']"))
+          .stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+  driver.findElement(By.xpath("//button[@class='rooms-search-widget__start-search ry-button--gradient-yellow']")).click();
+
+  Assert.assertTrue(driver.findElement(By.xpath("//span[@class='_container icon-20 input-button__error-icon']")).isDisplayed(), "The city error not found");
  }
 }
